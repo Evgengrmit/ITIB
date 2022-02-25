61c54d5d003d6a19350714bae6e9c8344b353cbd
Evgengrmit <evgengrmit@gmail.com>
2022-02-26 02:33:10 +0300

    replace pictures

--- LR1/main.go
+++ LR1/main.go
@@ -9,16 +9,27 @@ var Function = []int{0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1}
 
 func main() {
 	fmt.Println(strings.Repeat("-", 30), "FA 1st type", strings.Repeat("-", 30))
-	nw := NewNeuralNetwork(1, 0.3, Function)
-	nw.Study(Set)
-	Plot(nw.epochsArr, nw.errorsArr, "1.png")
-	fmt.Println(strings.Repeat("-", 30), "FA 2nd type", strings.Repeat("-", 30))
-	nw = NewNeuralNetwork(2, 0.3, Function)
-	nw.Study(Set)
-	Plot(nw.epochsArr, nw.errorsArr, "2.png")
-	FindMinSet()
-	nw = NewNeuralNetwork(2, 0.3, Function)
-	nw.Study(MinSet)
-	Plot(nw.epochsArr, nw.errorsArr, "3.png")
+	{
+		nw := NewNeuralNetwork(1, 0.3, Function)
+		nw.Study(Set)
+		Plot(nw.epochsArr, nw.errorsArr, "1.png")
+	}
+	fmt.Println("\n\n", strings.Repeat("-", 30), "FA 2nd type", strings.Repeat("-", 30))
+	{
+
+		nw := NewNeuralNetwork(2, 0.3, Function)
+		nw.Study(Set)
+		Plot(nw.epochsArr, nw.errorsArr, "2.png")
+	}
+	fmt.Println("\n\n", strings.Repeat("-", 25), "Get smallest values", strings.Repeat("-", 25))
+	{
+		FindMinSet()
+	}
+	fmt.Println("\n\n", strings.Repeat("-", 25), "FA smallest values", strings.Repeat("-", 25))
+	{
+		nw := NewNeuralNetwork(2, 0.3, Function)
+		nw.Study(MinSet)
+		Plot(nw.epochsArr, nw.errorsArr, "3.png")
+	}
 
 }
